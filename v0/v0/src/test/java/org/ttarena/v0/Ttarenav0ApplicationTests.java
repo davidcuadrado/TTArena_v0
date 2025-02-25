6c398615bad9670e5e93a3885c90c0dc889414eb
David <166518766+davidcuadrado@users.noreply.github.com>
2025-02-25 21:42:09 +0100

    feature: Spring Initializr configuration updload

---
+++ v0/v0/src/test/java/org/ttarena/v0/Ttarenav0ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ttarena.v0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ttarenav0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
