4d007a202556cfc6edcae7c313cae19febd4516f
David Cuadrado <dav.cuadrado@gmail.com>
2026-08-31 17:53:15 +0200

    fix: updated dependencies and config discrepancies

--- auth/build.gradle.kts
+++ auth/build.gradle.kts
@@ -35,7 +35,6 @@ dependencies {
 	implementation("io.jsonwebtoken:jjwt-impl:0.12.6")
 	implementation("io.jsonwebtoken:jjwt-jackson:0.12.6")
 	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
-	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,6 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
+}
 rootProject.name = "TTArena_v0"
 
 include("ability")
